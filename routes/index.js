5accca048fb71cd1a6ea3f6d4d789dee53f5b3c9
hoyuo <psh9907@gmail.com>
2016-05-29 08:38:21 +0900

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var express = require('express');
+var router = express.Router();
+
+mongoose.connect('mongodb://localhost/myDB');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback() {
+    console.log('OPEN');
+});
+
+
+var NFCSchema = mongoose.schema({
+    code: 'string'
+});
+
+var nfc = mongoose.model('NFC', NFCSchema);
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/* NFC Image Upload */
+
+router.post('/fileupload', function (req, res, next) {
+
+    var jsonContent = JSON.parse(req);
+
+    var Data = new nfc({
+        code: jsonContent.id
+    });
+
+    Data.save(function (err, data) {
+        if (err) {
+        }
+    });
+});
+
+
+module.exports = router;
